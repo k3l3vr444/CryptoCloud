cb883c46f767b9d0021d7404af6de75239716f6b
k3l3vr444 <ayukhanov_nikita@gmail.com>
2023-11-30 00:56:13 +0600

    initial commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='crypto_cloud',
     version='0.1',
-    description='https://cryptocloud.plus/ api',
+    description='https://cryptocloud.plus/ API wrapper',
     author='k3l3vr444',
     author_email='ayukanov.nikita@gmail.com',
     packages=['crypto_cloud', 'crypto_cloud.types'],  # same as name
